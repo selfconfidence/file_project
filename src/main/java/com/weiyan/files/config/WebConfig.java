259c5801bacd9875f67c16c2589ceeb5c4ab1477
selfconfidence <mynameisweiyan@gmail.com>
2019-05-31 14:51:39 +0800

    添加AES秘钥  -author wieyan

--- src/main/java/com/weiyan/files/config/WebConfig.java
+++ src/main/java/com/weiyan/files/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.weiyan.files.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -12,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
  * @date 2019/5/31 9:21
  */
 @Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class WebConfig  extends WebMvcConfigurationSupport {
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
