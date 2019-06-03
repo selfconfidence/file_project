8ced8ebe218032bdeb8c4ea24bc974169af40ac9
selfconfidence <mynameisweiyan@gmail.com>
2019-06-03 14:48:06 +0800

    后端拦截器完善  -author wieyan

--- src/main/java/com/weiyan/files/interceptor/FileInterceptor.java
+++ src/main/java/com/weiyan/files/interceptor/FileInterceptor.java
@@ -1,9 +1,12 @@
 package com.weiyan.files.interceptor;
 
+import com.weiyan.files.auto.JwtUtil;
+import io.jsonwebtoken.Claims;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,8 +19,24 @@ import javax.servlet.http.HttpServletResponse;
  */
 @Component
 public class FileInterceptor implements HandlerInterceptor {
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Logger logger = LoggerFactory.getLogger(FileInterceptor.class);
+        String auth = request.getHeader("AUTH");
+        try {
+            Claims claims = jwtUtil.parseJWT(auth);
+            String id = claims.getId();
+            if ("admin".equals(id)){
+                request.setAttribute("admin",id);
+            }
+        }catch (Exception e){
+            logger.error(e.getMessage());
+            return true;
+        }
+
         return true;
     }
 }
