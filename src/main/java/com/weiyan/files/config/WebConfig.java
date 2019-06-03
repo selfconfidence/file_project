bc32fe7e16b33f1b0a199c285ef9c4723c96f5ca
selfconfidence <mynameisweiyan@gmail.com>
2019-06-03 14:47:23 +0800

    后端拦截器填坑完善  -author wieyan

--- src/main/java/com/weiyan/files/config/WebConfig.java
+++ src/main/java/com/weiyan/files/config/WebConfig.java
@@ -1,10 +1,16 @@
 package com.weiyan.files.config;
 
+import com.weiyan.files.interceptor.FileInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author mister_wei
@@ -16,8 +22,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 @Configuration
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class WebConfig  extends WebMvcConfigurationSupport {
+    @Autowired
+    private FileInterceptor fileInterceptor;
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        List listPar = new ArrayList();
+        listPar.add("/static/**");
+        List interceptorList = new ArrayList();
+        interceptorList.add("/**");
+        registry.addInterceptor(fileInterceptor).addPathPatterns(interceptorList).excludePathPatterns(listPar);
     }
 }
