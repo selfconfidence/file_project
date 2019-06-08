f1a5726c9ca4d4a8dc67f72fbe381cae7b4b18f9
selfconfidence <mynameisweiyan@gmail.com>
2019-06-08 14:23:55 +0800

    完善 -author weiyan

--- src/main/resources/static/js/root.js
+++ src/main/resources/static/js/root.js
@@ -1,22 +1,37 @@
+
+function GetQueryString(name)
+{
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if(r!=null)return  unescape(r[2]); return null;
+}
+var jwt = '';
 var vue = new Vue({
     el: '#app',
     data: {
             deleteFileLocal:'',
-            jwt:''
+            jwt:'',
+            copyFiles:{
+                resourceFileLocal:'',
+                destFileLocal:''
+            }
+
     },
- /*   created:{
-      this.jwt =  this.$router.params.jwt;
-    },*/
+  created:() =>{
+        //去访问接口查看是否是登陆状态.
+        //仿制head头信息
+      jwt =  GetQueryString("auth");
+    },
     methods:{
         deleteFile(){
-            this.$confirm('此操作将永久删除该路径文件, 是否继续?', '提示', {
+            this.$confirm('此操作将备份该路径所有文件, 是否继续?', '提示', {
                 confirmButtonText: '确定',
                 cancelButtonText: '取消',
                 type: 'warning'
             }).then(() => {
-
                 //删除逻辑操作
-                axios.post('/file/deleteFiles',{"fileLocal":this.deleteFileLocal}).then( response => {
+                axios.post('/file/deleteFiles',{"fileLocal":this.deleteFileLocal,
+                },{headers: {'AUTH': jwt}}).then( response => {
                     if (response.data.flag) {
                         this.messageTitle("success",response.data.message);
                         this.$message({
@@ -41,6 +56,41 @@ var vue = new Vue({
                     message: '已取消删除'
                 });
             });
+        },
+        copyFile(){
+                //文件备份操作
+            this.$confirm('此操作备份文件下的所有文件, 是否继续?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(() => {
+                //删除逻辑操作
+                axios.post('/file/copyFiles',this.copyFiles,{headers: {'AUTH': jwt}}).then( response => {
+                    if (response.data.flag) {
+                        this.messageTitle("success",response.data.message);
+                        this.$message({
+                            type: 'success',
+                            message: response.data.message
+                        });
+                    }else {
+
+                        this.messageTitle("error",response.data.message);
+                        this.$message({
+                            type: 'error',
+                            message: response.data.message
+                        });
+                    }
+
+                })
+
+            }).catch((e) => {
+
+                this.$message({
+                    type: 'info',
+                    message: '已取消备份~'
+                });
+            });
+
         },
        messageTitle(type,message){
            const h = this.$createElement;
